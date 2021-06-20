9c7df27c546393cca69f53f34f4500104b5b2ad2
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-20 12:12:55 +0200

    Add files via upload

---
+++ arraysenobjects.js
@@ -0,0 +1,48 @@
+
+
+let name="Tamara";
+let age=43;
+
+
+let person={name:'Tamara', age:43, evaluations:[7,10,9]};
+
+console.log(person);
+
+
+
+person.name='Tamara'
+
+person['name']='Sinterklaas'
+
+console.log(person.name)
+
+console.log(person);
+
+console.log(person.evaluations)
+
+//waarde is [7,10,9]
+
+
+var array=['groen','blauw','rood','paars','geel'];
+
+console.log(array.length);
+
+
+console.log(array[0])
+
+console.log(array[4])
+
+var count=array.push('oranje');
+
+console.log(array)
+
+console.log(count)
+
+var count=array.push(6)
+console.log(array)
+
+var count=array.push({greeting:'hoi ik ben een object'});
+
+console.log(array[7]);
+
+
